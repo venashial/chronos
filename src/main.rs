ea6a33726f562b251f1293da971fc35f335c42e3
venashial <venashial.levo@aleeas.com>
2021-12-21 01:07:26 -0800

    Add /clear command

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,7 @@ use serenity::{
     model::{
         gateway::{Activity, Ready},
         interactions::{
-            application_command::{
-                ApplicationCommand,
-                ApplicationCommandOptionType,
-            },
+            application_command::{ApplicationCommand, ApplicationCommandOptionType},
             Interaction, InteractionApplicationCommandCallbackDataFlags, InteractionResponseType,
         },
     },
@@ -25,43 +22,49 @@ impl EventHandler for Handler {
         println!("{} is connected!", ready.user.name);
 
         ApplicationCommand::set_global_application_commands(&ctx.http, |commands| {
-            commands.create_application_command(|command| {
-                command
-                    .name("tz")
-                    .description("Append your timezone to your nickname")
-                    .create_option(|option| {
-                        option
-                            .name("timezone")
-                            .description("Your current timezone (UTC offset)")
-                            .kind(ApplicationCommandOptionType::String)
-                            .required(true)
-                            .add_string_choice("UTC-8", "-8")
-                            .add_string_choice("UTC-7", "-7")
-                            .add_string_choice("UTC-6", "-6")
-                            .add_string_choice("UTC-5", "-5")
-                            .add_string_choice("UTC-4", "-4")
-                            .add_string_choice("UTC-3", "-3")
-                            .add_string_choice("UTC-2", "-2")
-                            .add_string_choice("UTC-1", "-1")
-                            .add_string_choice("UTC+0", "+0")
-                            .add_string_choice("UTC+1", "+1")
-                            .add_string_choice("UTC+2", "+2")
-                            .add_string_choice("UTC+3", "+3")
-                            .add_string_choice("UTC+4", "+4")
-                            .add_string_choice("UTC+5", "+5")
-                            .add_string_choice("UTC+6", "+6")
-                            .add_string_choice("UTC+7", "+7")
-                            .add_string_choice("UTC+8", "+8")
-                            .add_string_choice("UTC+9", "+9")
-                            .add_string_choice("UTC+10", "+10")
-                            .add_string_choice("UTC+11", "+11")
-                            .add_string_choice("UTC+12", "+12")
-                            .add_string_choice("UTC-12", "-12")
-                            .add_string_choice("UTC-11", "-11")
-                            .add_string_choice("UTC-10", "-10")
-                            .add_string_choice("UTC-9", "-9")
-                    })
-            })
+            commands
+                .create_application_command(|command| {
+                    command
+                        .name("tz")
+                        .description("Append your timezone to your nickname")
+                        .create_option(|option| {
+                            option
+                                .name("timezone")
+                                .description("Your current timezone (UTC offset)")
+                                .kind(ApplicationCommandOptionType::String)
+                                .required(true)
+                                .add_string_choice("UTC-8", "-8")
+                                .add_string_choice("UTC-7", "-7")
+                                .add_string_choice("UTC-6", "-6")
+                                .add_string_choice("UTC-5", "-5")
+                                .add_string_choice("UTC-4", "-4")
+                                .add_string_choice("UTC-3", "-3")
+                                .add_string_choice("UTC-2", "-2")
+                                .add_string_choice("UTC-1", "-1")
+                                .add_string_choice("UTC+0", "+0")
+                                .add_string_choice("UTC+1", "+1")
+                                .add_string_choice("UTC+2", "+2")
+                                .add_string_choice("UTC+3", "+3")
+                                .add_string_choice("UTC+4", "+4")
+                                .add_string_choice("UTC+5", "+5")
+                                .add_string_choice("UTC+6", "+6")
+                                .add_string_choice("UTC+7", "+7")
+                                .add_string_choice("UTC+8", "+8")
+                                .add_string_choice("UTC+9", "+9")
+                                .add_string_choice("UTC+10", "+10")
+                                .add_string_choice("UTC+11", "+11")
+                                .add_string_choice("UTC+12", "+12")
+                                .add_string_choice("UTC-12", "-12")
+                                .add_string_choice("UTC-11", "-11")
+                                .add_string_choice("UTC-10", "-10")
+                                .add_string_choice("UTC-9", "-9")
+                        })
+                })
+                .create_application_command(|command| {
+                    command
+                        .name("clear")
+                        .description("Remove your timezone from your nickname")
+                })
         })
             .await
             .expect("Could not create application commands");
@@ -71,19 +74,8 @@ impl EventHandler for Handler {
     async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
         if let Interaction::ApplicationCommand(command) = interaction {
             let content = match command.data.name.as_str() {
-                "tz" => match &command.guild_id {
+                "tz" | "clear" => match &command.guild_id {
                     Some(_) => {
-                        let chosen_offset = command
-                            .data
-                            .options
-                            .get(0)
-                            .expect("Expected user option")
-                            .value
-                            .as_ref()
-                            .expect("Expected user object")
-                            .as_str()
-                            .expect("Could not parse option as string");
-
                         let member = &command.member.as_ref().expect("Member could not be found");
 
                         let regex = Regex::new(r" \(UTC[+\-]1?[0-9]\)$").unwrap();
@@ -93,13 +85,35 @@ impl EventHandler for Handler {
                             None => &member.user.name.as_str(),
                         };
 
-                        let base_nick = regex.replace(&current_nick, "");
+                        let base_nick = regex.replace(&current_nick, "").to_string();
 
-                        match member
-                            .edit(&ctx.http, |member| member.nickname(format!("{} (UTC{})", base_nick, chosen_offset)))
-                            .await {
-                            Ok(_) => "Changed your nickname".to_string(),
-                            _ => "Uh oh! I couldn't change your nickname. Make sure:\n1. I have permission to `Manage Nicknames`\n2. My role's position is __above__ yours. (I can't change admins' nicknames.)".to_string(),
+                        let new_nick = match command.data.name.as_str() {
+                            "tz" => {
+                                let chosen_offset = command
+                                    .data
+                                    .options
+                                    .get(0)
+                                    .expect("Expected command option")
+                                    .value
+                                    .as_ref()
+                                    .expect("Expected command option value")
+                                    .as_str()
+                                    .expect("Could not parse option value as string");
+
+                                format!("{} (UTC{})", base_nick, chosen_offset)
+                            },
+                            "clear" | _ => base_nick,
+                        };
+
+
+                        match current_nick == new_nick {
+                            true => "That command and option doesn't change your current nickname.".to_string(),
+                            false => match member
+                                .edit(&ctx.http, |member| member.nickname(&new_nick))
+                                .await {
+                                Ok(_) => format!("Your nickname has been changed to `{}`", new_nick),
+                                _ => "Uh oh! I couldn't change your nickname. Make sure:\n1. I have permission to `Manage Nicknames`\n2. My role's position is __above__ yours. (I can't change admins' nicknames.)".to_string(),
+                            }
                         }
                     }
                     None => "Uh oh! I can't change your nickname here.".to_string(),
